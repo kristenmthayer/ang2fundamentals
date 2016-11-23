f1bd23d15acd7bc846fc09b83777e2b019dbe11f
Kristen Thayer <kristenmariela@gmail.com>
2016-11-23 11:47:58 -0800

    updated repeat form

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
   template: `
     <div>
-    <input #myInput type="text">
+    <input #myInput type="text" [(ngModel)]="message">
     <button (click)="onClick(myInput.value)">Click me!</button>
     </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+@Input() message;
+
   onClick(value) {
     console.log('value ' + value);
   }
