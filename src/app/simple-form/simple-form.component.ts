055f110802d005dbb30118f3e130e28697144daf
Kristen Thayer <kristenmariela@gmail.com>
2016-11-23 12:27:23 -0800

    added css mousedown events

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -4,19 +4,29 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
   selector: 'app-simple-form',
   template: `
     <div>
-    <input #myInput type="text" [(ngModel)]="message">
+    <input #myInput 
+    type="text" 
+    [(ngModel)]="message" 
+    [ngClass]="{mousedown:isMousedown}"
+    (mousedown)="isMousedown = true"
+    (mouseup)="isMousedown = false"
+    (mouseleave)="isMousedown = false"
+    >
     <button (click)="update.emit({text:message})">Click me!</button>
     </div>
   `,
-  styles: []
+  styles: [
+    `
+    .mousedown{
+      border: 2px solid blue;
+    }
+    `
+  ]
 })
 export class SimpleFormComponent implements OnInit {
 @Input() message;
 @Output()update = new EventEmitter();
-
-
-
-
+isMouseDown: boolean = false;
   constructor() { }
 
   ngOnInit() {
